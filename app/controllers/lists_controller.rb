3c3dd24f78e3fa84e429c1b9215582c93fb77aad
Shermaine Sng <shersmallsng@gmail.com>
2021-11-26 18:45:20 +0800

    draft

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show]
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
@@ -18,6 +18,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.create!(list_params)
+    redirect_to lists_path
+  end
+
+  def destroy
+    @list.destroy
     redirect_to lists_path
   end
 
